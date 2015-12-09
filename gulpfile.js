f53b4e2f03ec9e944d4fb82c53b32081db4adb6e
alberto acevdedo <vascoace@aol.com>
2015-12-08 21:11:56 -0500

    Modified gulpFile and package.json based on changes pulled by kaktus40

--- gulpfile.js
+++ gulpfile.js
@@ -6,13 +6,8 @@ var less = require('gulp-less');
 var minifyCSS = require('gulp-minify-css');
 var notify = require('gulp-notify');
 var gutil = require('gulp-util');
-var clean = require('gulp-clean');
-var watch = require('gulp-watch');
-var ername = require('gulp-rename');
+var del = require('del');
 var eventStream = require('event-stream');
-var order = require('gulp-order');
-var gignore = require('gulp-ignore');
-var gjshint = require('gulp-jshint');
 var gfilter = require('gulp-filter');
 
 gulp.task('bundle-minified',['less'],  function ()
@@ -21,19 +16,19 @@ gulp.task('bundle-minified',['less'],  function ()
     var almond = gulp.src("lib/ThirdParty/almond.js");
     var cesiumNavigation = gulp.src('**/*.js')
            // .pipe(jshint())
-            .pipe(filter)
-            .pipe(amdOptimize('NavigationStartup',
-            {
-                configFile: "NavigationStartup.js",
-                baseUrl: '../cesium-navigation/'
-            }
-    ));
-    return eventStream.merge(almond, cesiumNavigation)
-    .pipe(concat("cesium-navigation.js"))
-    .pipe(uglify())
-    .pipe(gulp.dest("dist/cesium-navigation"))
-    .pipe(notify('eventStream merging build completed'));
-});
+           .pipe(filter)
+           .pipe(amdOptimize('NavigationStartup',
+           {
+            configFile: "NavigationStartup.js",
+            baseUrl: '../cesium-navigation/'
+        }
+        ));
+           return eventStream.merge(almond, cesiumNavigation)
+           .pipe(concat("cesium-navigation.js"))
+           .pipe(uglify())
+           .pipe(gulp.dest("dist/cesium-navigation"))
+           .pipe(notify('eventStream merging build completed'));
+       });
 
 gulp.task('bundle-unminified',['less'],  function ()
 {
@@ -41,37 +36,43 @@ gulp.task('bundle-unminified',['less'],  function ()
     var almond = gulp.src("lib/ThirdParty/almond.js");
     var cesiumNavigation = gulp.src('**/*.js')
            // .pipe(jshint())
-            .pipe(filter)
-            .pipe(amdOptimize('NavigationStartup',
-            {
-                configFile: "NavigationStartup.js",
-                baseUrl: '../cesium-navigation/'
-            }
-    ));
-    return eventStream.merge(almond, cesiumNavigation)
-    .pipe(concat("cesium-navigation.js"))
+           .pipe(filter)
+           .pipe(amdOptimize('NavigationStartup',
+           {
+            configFile: "NavigationStartup.js",
+            baseUrl: '../cesium-navigation/'
+        }
+        ));
+           return eventStream.merge(almond, cesiumNavigation)
+           .pipe(concat("cesium-navigation.js"))
    // .pipe(uglify())
-    .pipe(gulp.dest("dist/cesium-navigation"))
-    .pipe(notify('eventStream merging build completed'));
+   .pipe(gulp.dest("dist/cesium-navigation"))
+   .pipe(notify('eventStream merging build completed'));
 });
 
 
 gulp.task('less', ['cleanDist'], function ()
 {
     gulp.src('lib/Styles/less/cesium-navigation.less')
-            .pipe(less({compress: true}).on('error', gutil.log))
-            .pipe(minifyCSS({keepBreaks: false}))
-            .pipe(gulp.dest('dist/cesium-navigation'))
-            .pipe(notify('Less Compiled, compressed and moinified'));
+    .pipe(less({compress: true}).on('error', gutil.log))
+    .pipe(minifyCSS({keepBreaks: false}))
+    .pipe(gulp.dest('dist/cesium-navigation'))
+    .pipe(notify('Less Compiled, compressed and minified'));
 });
 
 gulp.task('cleanDist', function () {
-    return gulp.src('dist', {read: false})
-            .pipe(clean({force: true}));
+    del.sync(['./dist'], function(err, deletedFiles) {
+        if(deletedFiles.length) {
+          gutil.log('Deleted', gutil.colors.red(deletedFiles.join(' ,')) );
+      } else {
+          gutil.log(gutil.colors.yellow('dist directory empty - nothing to delete'));
+      }
+  });
 });
-gulp.task('default', ['cleanDist', 'less', 'bundle-minified'], function () {
-    });
 
-    
+gulp.task('default', ['cleanDist', 'less', 'bundle-minified'], function () {
+});
+
+
 gulp.task('release-unminified', ['cleanDist', 'less', 'bundle-unminified'], function () {
 });
