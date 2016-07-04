617e3067b735ad6ea9c1a5414e80774c1345bb1b
Gabriel Cordes <gabriel.cordes@h-d-gmbh.de>
2016-07-04 14:52:28 +0200

    support of tracked entities
    in 3D mode zooming is now with animation but not when an entity is tracked

--- Source/ViewModels/ResetViewNavigationControl.js
+++ Source/ViewModels/ResetViewNavigationControl.js
@@ -5,110 +5,114 @@ define([
     'ViewModels/NavigationControl',
     'SvgPaths/svgReset'
 ], function (
-        defined,
-        Camera,
-        NavigationControl,
-        svgReset)
-    {
+    defined,
+    Camera,
+    NavigationControl,
+    svgReset) {
         'use strict';
 
+    /**
+     * The model for a zoom in control in the navigation control tool bar
+     *
+     * @alias ResetViewNavigationControl
+     * @constructor
+     * @abstract
+     *
+     * @param {Terria} terria The Terria instance.
+     */
+    var ResetViewNavigationControl = function (terria) {
+        NavigationControl.apply(this, arguments);
+
         /**
-         * The model for a zoom in control in the navigation control tool bar
-         *
-         * @alias ResetViewNavigationControl
-         * @constructor
-         * @abstract
-         *
-         * @param {Terria} terria The Terria instance.
+         * Gets or sets the name of the control which is set as the control's title.
+         * This property is observable.
+         * @type {String}
          */
-        var ResetViewNavigationControl = function (terria)
-            {
-                NavigationControl.apply(this, arguments);
+        this.name = 'Reset View';
 
-                /**
-                 * Gets or sets the name of the control which is set as the control's title.
-                 * This property is observable.
-                 * @type {String}
-                 */
-                this.name = 'Reset View';
+        /**
+         * Gets or sets the svg icon of the control.  This property is observable.
+         * @type {Object}
+         */
+        this.svgIcon = svgReset;
 
-                /**
-                 * Gets or sets the svg icon of the control.  This property is observable.
-                 * @type {Object}
-                 */
-                this.svgIcon = svgReset;
+        /**
+         * Gets or sets the height of the svg icon.  This property is observable.
+         * @type {Integer}
+         */
+        this.svgHeight = 15;
 
-                /**
-                 * Gets or sets the height of the svg icon.  This property is observable.
-                 * @type {Integer}
-                 */
-                this.svgHeight = 15;
+        /**
+         * Gets or sets the width of the svg icon.  This property is observable.
+         * @type {Integer}
+         */
+        this.svgWidth = 15;
 
-                /**
-                 * Gets or sets the width of the svg icon.  This property is observable.
-                 * @type {Integer}
-                 */
-                this.svgWidth = 15;
+        /**
+         * Gets or sets the CSS class of the control. This property is observable.
+         * @type {String}
+         */
+        this.cssClass = "navigation-control-icon-reset";
 
-                /**
-                 * Gets or sets the CSS class of the control. This property is observable.
-                 * @type {String}
-                 */
-                this.cssClass = "navigation-control-icon-reset";
+    };
 
-            };
+    ResetViewNavigationControl.prototype = Object.create(NavigationControl.prototype);
 
-        ResetViewNavigationControl.prototype = Object.create(NavigationControl.prototype);
+    ResetViewNavigationControl.prototype.resetView = function () {
+        //this.terria.analytics.logEvent('navigation', 'click', 'reset');
 
-        ResetViewNavigationControl.prototype.resetView = function ()
-            {
-                //this.terria.analytics.logEvent('navigation', 'click', 'reset');
+        var scene = this.terria.scene;
 
-                this.isActive = true;
+        var sscc = scene.screenSpaceCameraController;
+        if (!sscc.enableInputs) {
+            return;
+        }
 
-                var camera = this.terria.scene.camera;
+        this.isActive = true;
 
-                //reset to a default position or view defined in  the options
-                if (this.terria.options.defaultResetView)
-                    {
-                        if (this.terria.options.defaultResetView && this.terria.options.defaultResetView instanceof  Cesium.Cartographic)
-                            {
-                                camera.flyTo({
-                                    destination: Cesium.Ellipsoid.WGS84.cartographicToCartesian(this.terria.options.defaultResetView)
-                                });
-                            } else   if (this.terria.options.defaultResetView && this.terria.options.defaultResetView instanceof  Cesium.Rectangle)
-                            {
-                                try
-                                    {
-                                        Cesium.Rectangle.validate(this.terria.options.defaultResetView);
-                                        camera.flyTo({
-                                            destination: this.terria.options.defaultResetView
-                                        });
-                                    } catch (e)
-                                    {
-                                        console.log("Cesium-navigation/ResetViewNavigationControl:   options.defaultResetView Cesium rectangle is  invalid!");
-                                    }
-                            }
-                    } 
-                    else if (typeof camera.flyHome === "function")
-                    {
-                        camera.flyHome(1);
-                    } else
-                    {
-                        camera.flyTo({'destination': Camera.DEFAULT_VIEW_RECTANGLE, 'duration': 1});
+        var camera = scene.camera;
+
+        if (defined(this.terria.trackedEntity)) {
+            // when tracking do not reset to default view but to default view of tracked entity
+            var trackedEntity = this.terria.trackedEntity;
+            this.terria.trackedEntity = undefined;
+            this.terria.trackedEntity = trackedEntity;
+        } else {
+            // reset to a default position or view defined in the options
+            if (this.terria.options.defaultResetView) {
+                if (this.terria.options.defaultResetView && this.terria.options.defaultResetView instanceof Cesium.Cartographic) {
+                    camera.flyTo({
+                        destination: Cesium.Ellipsoid.WGS84.cartographicToCartesian(this.terria.options.defaultResetView)
+                    });
+                } else if (this.terria.options.defaultResetView && this.terria.options.defaultResetView instanceof Cesium.Rectangle) {
+                    try {
+                        Cesium.Rectangle.validate(this.terria.options.defaultResetView);
+                        camera.flyTo({
+                            destination: this.terria.options.defaultResetView
+                        });
+                    } catch (e) {
+                        console.log("Cesium-navigation/ResetViewNavigationControl:   options.defaultResetView Cesium rectangle is  invalid!");
                     }
-                this.isActive = false;
-            };
+                }
+            }
+            else if (typeof camera.flyHome === "function") {
+                camera.flyHome(1);
+            } else {
+                camera.flyTo({'destination': Camera.DEFAULT_VIEW_RECTANGLE, 'duration': 1});
+            }
+        }
+        this.isActive = false;
+    };
 
-        /**
-         * When implemented in a derived class, performs an action when the user clicks
-         * on this control
-         * @abstract
-         * @protected
-         */
-        ResetViewNavigationControl.prototype.activate = function ()
-            {
-                this.resetView();
-            };
-        return ResetViewNavigationControl;
-    });
+    /**
+     * When implemented in a derived class, performs an action when the user clicks
+     * on this control
+     * @abstract
+     * @protected
+     */
+    ResetViewNavigationControl.prototype.activate = function () {
+        this.resetView();
+    };
+
+    return ResetViewNavigationControl;
+});
