542840af189d6850e1fba6bcf8e8adf48ffc7a1e
Gabriel Cordes <gabriel.cordes@h-d-gmbh.de>
2016-10-19 16:19:05 +0200

    version 1.1.3 (the core has not been changed but only the build script and the examples) * improved AMD compatible version: it automatically checks if Cesium is accessible via the source directory (i.e. Cesium is pointing to <cesium>/Source folder), via the built version of Cesium (i.e. Cesium is pointing to <cesium>/Build/Cesium/Cesium.js) or globally (i.e. Cesium does not point anywhere but Cesium was loaded without AMD) * added new example where AMD is used but without require.optimize * slightly adapted examples to fit new version

--- Examples/Source/mainConfig.js
+++ Examples/Source/mainConfig.js
@@ -3,7 +3,15 @@ requirejs.config({
     paths: {
         // IMPORTANT: this path has to be set because
         //  viewerCesiumNavigationMixin uses 'Cesium/...' for dependencies
-        Cesium: "../node_modules/cesium/Source",
-        viewerCesiumNavigationMixin: "../dist/amd/viewerCesiumNavigationMixin.min"
+        Cesium: "../node_modules/cesium/Build/Cesium/Cesium",
+        viewerCesiumNavigationMixin: "../dist/amd/viewerCesiumNavigationMixin"
+    },
+    shim: {
+        "Cesium": {
+            exports: "Cesium",
+//            deps: [
+//                "require-css!../../node_modules/cesium/Build/Cesium/Widgets/widgets.css"
+//            ]
+        }
     }
 });
